a83ddc477165e8032b347f16e4143f8680aea441
aprilinusa <62549066+aprilinusa@users.noreply.github.com>
2020-03-24 11:54:42 +0800

    Update test.java

--- test.java
+++ test.java
@@ -12,13 +12,13 @@ public class Store {
       } else if (product == "Sulfuras") {
         assert Quality[SellIn] == Quality[SellIn+1]
       } else if (product == "Backstage pass") { 
-	      if (SellIn < 0) { 
-          assert Quality[SellIn] == 0
-        } else if (SellIn < 5) {
-          assert Quality[SellIn] == Quality[SellIn+1] + 3 
-	      } else if (SellIn < 10) {
-          assert Quality[SellIn] == Quality[SellIn+1] + 2
-	      } else { Quality[SellIn] == Quality[SellIn+1] }
+	  if (SellIn < 0) { 
+            assert Quality[SellIn] == 0
+          } else if (SellIn < 5) {
+            assert Quality[SellIn] == Quality[SellIn+1] + 3 
+	  } else if (SellIn < 10) {
+            assert Quality[SellIn] == Quality[SellIn+1] + 2
+	  } else { Quality[SellIn] == Quality[SellIn+1] }
       } else {
         if (SellIn < 0) {  // 价值双倍下降
           assert Quality[SellIn] == Quality[SellIn+1] / 2
